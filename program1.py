54077f787641bc386f6146a65e9a063eb5abf39a
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-03-30 23:20:59 +0500

    printing return

--- program1.py
+++ program1.py
@@ -1,5 +1,16 @@
 print("This is my first program!")
 
-myName("Ali")   # You can't call any function before decleration
-def myName(name):
-    print("My name is: ", name)
+# myName("Ali")   # You can't call any function before decleration
+# def myName(name):
+#     print("My name is: ", name)
+
+def intro(fn, ln):
+    print("I am ", fn, ln)
+    return 123
+
+intro("M", "Awais")
+
+# Return didn't print
+
+out = intro("M", "Ali")
+print(out)  # Now return 123 print
