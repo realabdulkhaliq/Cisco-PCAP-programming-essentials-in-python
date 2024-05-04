7d2945c54e08b59b1cd4e79f3f95494c9424b026
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2024-05-04 22:00:27 +0500

    error

--- program3.py
+++ program3.py
@@ -2,5 +2,9 @@
 # print(x(5))
 
 # Multiply argument a with argument b and return the result:
-x = lambda a, b : a * b
-print(x(5, 6))
+# x = lambda a, b : a * b
+# print(x(5, 6))
+
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
