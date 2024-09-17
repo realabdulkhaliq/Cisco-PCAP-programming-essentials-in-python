4baee208dae3f77b17e7728e0cbec746d4dd7cce
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-17 20:08:31 +0500

    print double and single quotes

---
+++ Python_Essentials_1/Module2/print_double_single_quote.py
@@ -0,0 +1,18 @@
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
+
+
+#       print('"Greg's book."')
+#       Produce an error
+#       SyntaxError: unterminated string literal (detected at line 1)
+
+
+print("I like \"Monty Python\"") # OK but Not recommended
+print('I like "Monty Python"')
+
+# An empty string still remains a string:
+
+''
+""
