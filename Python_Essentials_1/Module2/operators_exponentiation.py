adeec8530623333b9152c67a961aa1a24aa31f82
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-24 22:32:01 +0500

    operators exponents

---
+++ Python_Essentials_1/Module2/operators_exponentiation.py
@@ -0,0 +1,7 @@
+print(2 ** 3) # Int
+print(2 ** 3.) # Float
+print(2. ** 3) # Float
+print(2. ** 3.) # Float
+
+# when both ** arguments are integers, the result is an integer, too;
+# when at least one ** argument is a float, the result is a float, too.
