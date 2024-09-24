04e9bee9b6d772c215bd73e54824149eee7778e8
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-24 22:30:56 +0500

    number in exponents

---
+++ Python_Essentials_1/Module2/number_exponent.py
@@ -0,0 +1,23 @@
+print(3e8)
+print(3E8)
+
+print(3E8)
+print(3E15)
+print(3E16)
+
+# Exponent up to 15
+print("==============")
+print(3E-4)
+print(3E-5)
+print("==============")
+# Negative Exponent up to 4
+print(0.0005)
+print(0.00005)
+print(0.0000000005)
+
+
+# the exponent (the value after the E) has to be an integer;
+# the base (the value in front of the E) may be either an integer or a float.
+
+print(6.62607E-34)
+print(0.0000000000000000000001)
