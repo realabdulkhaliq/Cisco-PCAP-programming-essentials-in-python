639001d9c345996275960d3cdfcc007439ba1611
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-24 22:31:31 +0500

    operators

---
+++ Python_Essentials_1/Module2/operators.py
@@ -0,0 +1,17 @@
+print(2+2)
+#* An operator is a symbol of the programming language, which is able to operate on the values.
+# NOTE: Data and operators when connected together form expressions. 
+# The simplest expression is a literal itself.
+
+print(-4 + 4)
+print(-4. + 8) # Float
+print(-4. + 8.0) # Float
+
+# In subtracting applications, the minus operator expects two arguments: 
+# the left (a minuend in arithmetical terms) and right (a subtrahend).
+print("=======")
+print(-4 - 4)
+print(4. - 8)
+print(-1.1) # unary
+print(+2) # unary
+
