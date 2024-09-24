6e7836475c1489508a3360d83b0e73803d21787c
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-24 20:59:18 +0500

    String and Integer Literals

--- Python_Essentials_1/Module2/String_and_Integer_Literals.py
+++ Python_Essentials_1/Module2/String_and_Integer_Literals.py
@@ -1,3 +1,4 @@
+# A literal is data whose values are determined by the literal itself.
 print("2")
 print(2)
 
@@ -5,4 +6,14 @@ print(2)
 print(2+2)
 print(2.2)
 
-print("This is in decimal", 10)
+# *integers*, that is, those which are devoid of the fractional part;
+# and *floating-point* numbers (or simply floats), that contain (or are able to contain) the fractional part.
+# The characteristic of the numeric value which determines its kind, range, and application, is called the type.
+print(type(2))
+
+#! print(11 11 11) It's prohibited.
+
+# Python does allow, though, is the use of underscores in numeric literals.
+print(11_111_111) # Output 11111111
+print(11_111_111.11) # Output 11111111.11
+print(-11_111_111) # Negative Number
