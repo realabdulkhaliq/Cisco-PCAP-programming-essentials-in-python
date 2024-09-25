9ca96c9401b29db8c50d4e2a8bf40203d9dddddd
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-25 22:40:41 +0500

    Variables

---
+++ Python_Essentials_1/Module2/variables.py
@@ -0,0 +1,14 @@
+# NOTE: A variable comes into existence as a result of assigning a value to it.
+
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+
+var1 = "3.8.5"
+print("Python version: " + var1)
+
+var2 = 3.8
+print("Python version: " + str(var2))
