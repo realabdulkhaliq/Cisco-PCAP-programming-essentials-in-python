8b55384c1b45d83d2fab3aee6230f61341a60c08
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 05:40:52 +0500

    Variable Algebra

---
+++ Python_Essentials_1/Module2/variable_algebra1.py
@@ -0,0 +1,13 @@
+# Take a look at the code in the editor: it reads a float value, puts it into a variable named x, 
+# and prints the value of a variable named y. Your task is to complete the code in order to 
+# evaluate the following expression:
+
+x =  0.0
+x = float(x)
+y = 3 * x ** 3 - 2 *x ** 2 + 3 * x - 1
+
+print("y =", y)
+x =  1
+x = float(x)
+y = 3 * x ** 3 - 2 *x ** 2 + 3 * x - 1
+print("y =", y)
