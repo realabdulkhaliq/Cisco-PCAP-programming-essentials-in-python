e5620271c4114f9b91f39409b4e0f667962f1cfa
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 18:47:12 +0500

    larger of three numbers example 1

---
+++ Python_Essentials_1/Module3/larger_of_three_numbers_max_fun.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+ 
+# Check which one of the numbers is the greatest
+# and pass it to the largest_number variable.
+ 
+largest_number = max(number1, number2, number3)
+ 
+# Print the result.
+print("The largest number is:", largest_number)
+
+
+# NOTE: By the same fashion, you can use the min() function to return the lowest number. 
