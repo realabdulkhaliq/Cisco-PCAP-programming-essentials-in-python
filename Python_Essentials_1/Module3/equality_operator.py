302f71801bb82c396c3db52149b2c04cb8b43f9f
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 18:46:55 +0500

    equality operator

---
+++ Python_Essentials_1/Module3/equality_operator.py
@@ -0,0 +1,15 @@
+print(2==2) # True
+print(2==2.) # True
+
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+var = 0  # Assigning 0 to var
+print(var != 0)
+ 
+var = 1  # Assigning 1 to var
+print(var != 0)
+ 
