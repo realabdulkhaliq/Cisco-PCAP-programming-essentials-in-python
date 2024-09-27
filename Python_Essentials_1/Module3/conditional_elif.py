12e2b6d90556fa8fd869a63849c5f5ad9538239c
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 18:45:31 +0500

    Conditional elif example

---
+++ Python_Essentials_1/Module3/conditional_elif.py
@@ -0,0 +1,16 @@
+x = "1"
+ 
+if x == 1:
+    print("one")
+elif x == "1":
+    if int(x) > 1:
+        print("two")
+    elif int(x) < 1:
+        print("three")
+    else:
+        print("four")
+if int(x) == 1:
+    print("five")
+else:
+    print("six")
+ 
