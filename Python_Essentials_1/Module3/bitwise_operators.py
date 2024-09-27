d118cf9d5ba0c846fdf6aa422e397f68bc61e82c
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 22:13:10 +0500

    Bitwise Operators

---
+++ Python_Essentials_1/Module3/bitwise_operators.py
@@ -0,0 +1,17 @@
+x = 4
+y = 1
+ 
+a = x & y
+b = x | y
+c = ~x  # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+ 
+print(a, b, c, d, e, f)
+
+
+var = 17
+var_right = var >> 1
+var_left = var << 2
+print(var, var_left, var_right)
