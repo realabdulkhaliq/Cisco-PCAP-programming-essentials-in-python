3221d7d0062f608aa21a5fba7aaef4b6d34a8346
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 18:43:55 +0500

    comparison_operators

---
+++ Python_Essentials_1/Module3/comparison_operators.py
@@ -0,0 +1,6 @@
+print("black_sheep" > "white_sheep") # False by Use Unicode values
+
+ans = "A" > "B"
+print(ans) # False by Use Unicode values
+ans = "A" >= "B"
+print(ans) # False by Use Unicode values
