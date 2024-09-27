1af4caf4a2dd4108fffeb258cd1a620bdf921932
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 06:42:53 +0500

    arithmetic operations

---
+++ Python_Essentials_1/Module2/iput_arithmetic_operations.py
@@ -0,0 +1,10 @@
+a = float(input("Enter value: "))
+b = float(input("Enter value: "))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("\nThat's all, folks!")
+print(sum, sub, mul, div)
