e835019ad76ae28c581746f303689e9b15b69ff3
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 06:43:06 +0500

    m2 quiz solution

---
+++ Python_Essentials_1/Module2/m2_quiz.py
@@ -0,0 +1,58 @@
+# Q1
+# x = 1
+# y = 2
+# z = x # z = 1
+# x = y # x = 2
+# y = z # y = 1
+# print(x, y) # 2, 1
+
+# Q2
+# x = int(input()) # 2
+# y = int(input()) # 4
+ 
+# x = x // y # 2//4 = 0.5 = 0 
+# y = y // x # 4//0  error
+ 
+# print(y)
+
+
+# Q3
+# a = int(input()) # 2
+# b = int(input()) # 4
+ 
+# a = a / b # 2/4 = 0.5
+# b = b / a # 4/0.5 = 8.0
+ 
+# print(b)
+
+
+#Q4
+# x = int(input()) # 11
+# y = int(input()) # 4
+ 
+# x = x % y # 11%4 = 3
+# x = x % y #3%4 = 3
+# y = y % x # 4%3 = 1
+ 
+# print(y)
+ 
+# Q5
+# x = input() # string 3
+# y = int(input()) # int 6
+ 
+# print(x * y) # replication
+
+#Q6
+# z = y = x = 1
+# print(x, y, z, sep='*')
+
+# Q7
+# y = 2 + 3 * 5.
+# print(Y)
+
+# Q8
+x = 1 / 2 + 3 // 3 + 4 ** 2
+# = 1/2 + 3//3 + 16
+# = 0.5 + 1 + 16
+# 17.5
+print(x)
