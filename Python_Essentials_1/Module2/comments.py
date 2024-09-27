c03b395a5bf8f5edaa01cddf412a84d3b81342ed
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-27 05:39:11 +0500

    Comments

---
+++ Python_Essentials_1/Module2/comments.py
@@ -0,0 +1,9 @@
+# If you'd like to quickly comment or uncomment multiple lines of code, 
+# select the line(s) you wish to modify and use the following keyboard shortcut: 
+# CTRL + / (Windows) or CMD + / (Mac OS).
+
+uncomment_me = 1
+uncomment_me_too = 3
+uncomment_me_also = 5
+
+print(uncomment_me, uncomment_me_too, uncomment_me_also, sep="\n")
