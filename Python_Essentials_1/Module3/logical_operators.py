e0ae1cfbb6495f0ecdcd2574fc073cd99dbe4aaf
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-28 09:40:03 +0500

    logical operators

---
+++ Python_Essentials_1/Module3/logical_operators.py
@@ -0,0 +1,14 @@
+# Example 1:
+var = 1
+print(var > 0)
+print(not (var <= 0))
+ 
+ 
+# Example 2:
+print(var != 0)
+print(not (var == 0))
+
+
+i = 1 
+j = not not i # Flase True => True
+print("j",j)
