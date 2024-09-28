08f8e0435713816f781cc9d79770daf289425909
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-28 09:39:35 +0500

    Simple for loop example

---
+++ Python_Essentials_1/Module3/for_simple_for_loop.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print("The value of i is currently", i)
+
+for i in range(2, 8, 3):
+    print("The value of i is now", i)
+
+for i in range(1, 1):
+    print("The value of i is ", i) # there will be no output
+
+for i in range(2, 1):
+    print("The value of i is currently", i) # there will be no output
