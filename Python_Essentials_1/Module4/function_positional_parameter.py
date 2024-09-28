918139f908bb61e372604f9986fa11c0bae7993f
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-28 21:31:23 +0500

    Function with positional parameters

---
+++ Python_Essentials_1/Module4/function_positional_parameter.py
@@ -0,0 +1,6 @@
+def introduction(first_name, last_name):
+    print("Hello, my name is", first_name, last_name)
+
+introduction("Luke", "Skywalker")
+introduction("Jesse", "Quick")
+introduction("Clark", "Kent") 
