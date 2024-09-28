86fd9249b6d02fe88d58b3deca9a6d1fb6f910d8
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-28 16:20:52 +0500

    List Append & Insert

---
+++ Python_Essentials_1/Module3/list_append_insert.py
@@ -0,0 +1,13 @@
+my_list = []  # Creating an empty list.
+
+for i in range(5):
+    my_list.append(i + 1)
+
+print(my_list)
+
+my_list2: list = []  # Creating an empty list.
+ 
+for i in range(5):
+    my_list2.insert(0, i + 1)
+ 
+print(my_list2)
