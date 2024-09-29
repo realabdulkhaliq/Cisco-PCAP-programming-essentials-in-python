9f7d13fac8b0365449244b1f2ce54d2b5529f382
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-29 18:07:38 +0500

    function return with an expression

---
+++ Python_Essentials_1/Module4/function_return_with_expression.py
@@ -0,0 +1,11 @@
+def boring_function():
+    print("'Boredom Mode' ON.")
+    return 123
+
+# x = boring_function()
+
+# print("The boring_function has returned its result. It's:", x)
+
+print("This lesson is interesting!")
+boring_function()
+print("This lesson is boring...")
