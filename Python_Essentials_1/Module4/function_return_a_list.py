6b01511a12115af6a9326af086fd2d0dfddfd223
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-29 18:07:19 +0500

    function return a list

---
+++ Python_Essentials_1/Module4/function_return_a_list.py
@@ -0,0 +1,10 @@
+def strange_list_fun(n):
+    strange_list = []
+    
+    for i in range(0, n):
+        # strange_list.insert(0, i)
+        strange_list.append(i)
+    
+    return strange_list
+
+print(strange_list_fun(5))
