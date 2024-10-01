d1d25a049e0a819c2629683e0f54d1b60f6de60c
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-10-01 17:28:37 +0500

    Function Right Angle Triangle

---
+++ Python_Essentials_1/Module4/fun_is_right_angle_triangle.py
@@ -0,0 +1,15 @@
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+def is_a_right_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return False
+    if c > a and c > b:
+        return c ** 2 == a ** 2 + b ** 2
+    if b > a and b > c:
+        return b ** 2 == a ** 2 + c ** 2
+    if a > b and a > c:
+        return a ** 2 == b ** 2 + c ** 2
+print(is_a_right_triangle(5, 3, 4))
+print(is_a_right_triangle(1, 3, 4))
