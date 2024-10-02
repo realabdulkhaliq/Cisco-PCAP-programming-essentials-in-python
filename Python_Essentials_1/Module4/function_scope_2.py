567bdde098305996a51a8203ecab5e458e728920
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-10-02 22:49:24 +0500

    Function Scope Example 2

---
+++ Python_Essentials_1/Module4/function_scope_2.py
@@ -0,0 +1,14 @@
+var = 2
+def mult_by_var(x):
+    return x * var
+
+print(mult_by_var(7))    # outputs: 14
+
+print("=========")
+
+def mult(x):
+    var = 5
+    return x * var
+
+print(mult(7))    # outputs: 35
+
