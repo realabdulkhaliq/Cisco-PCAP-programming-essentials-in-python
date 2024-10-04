0475a3c921238c14810740d63f61f991a0ebf86c
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-10-04 18:55:21 +0500

    Tuple Exampels

---
+++ Python_Essentials_1/Module4/tuple_examples_2.py
@@ -0,0 +1,9 @@
+my_tuple = (1, 2, True, "a string", (3, 4), [5, 6], None)
+print(my_tuple)
+
+my_list = [1, 2, True, "a string", (3, 4), [5, 6], None]
+print(my_list)
+
+my_tuple2 = (1, 2.0, "string", [3, 4], (5, ), True)
+print(my_tuple2[3])    # outputs: [3, 4]
+
