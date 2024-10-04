12f1e277b60a94995a03d1784a8653c49e4b643a
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-10-04 18:58:20 +0500

    Error deal with more than one exce[ption

---
+++ Python_Essentials_1/Module4/err_deal_with_more_than_one_exception.py
@@ -0,0 +1,22 @@
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)        
+except ValueError:
+    print('I do not know what to do.')    
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.') 
+
+# if one of the branches is executed, all the other branches remain idle.
+
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)        
+except ValueError:
+    print('I do not know what to do.')    
+except ZeroDivisionError:
+    print('Division by zero is not allowed in our Universe.')    
+except:
+    print('Something strange has happened here... Sorry!')
+
+# except branch, but this time it has no exception name specified – we can say it's anonymous or 
+# (what is closer to its actual role) it's the default.
